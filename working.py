ae62a00739486cc1d1c6537cf28fcaf322998bcd
admiralpunk <aniketkolte79@gmail.com>
2025-02-11 12:24:31 +0530

    done with basics

--- working.py
+++ working.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/about")
+def about():
+    return {"About": "Data"}
+
+@app.get("/contact")
+def contact():
+    return {"message": "Contact page"}
+    
